3938c0bc6286750d5ad98da192b8ebcc97178408
Inju Michorius <inju@hotmail.nl>
2024-03-20 17:15:27 +0100

    Initial commit, everything works as expected.

--- server.js
+++ server.js
@@ -1,58 +1,48 @@
-/* Losjes gebaseerd op https://socket.io/get-started/chat */
+const path = require('path');
+const { Server } = require('socket.io');
+const { createServer } = require('http');
+const express = require('express');
 
-import * as path from 'path'
-
-import { Server } from 'socket.io'
-import { createServer } from 'http'
-import express from 'express'
-
-const app = express()
-const http = createServer(app)
+const app = express();
+const http = createServer(app);
 const ioServer = new Server(http, {
   connectionStateRecovery: {
-    // De tijdsduur voor recovery bij disconnect
     maxDisconnectionDuration: 2 * 60 * 1000,
-    // Of middlewares geskipped moeten worden bij recovery (ivm login)
     skipMiddlewares: true,
   },
-})
-const port = process.env.PORT || 4242
-const historySize = 50
+});
+const port = process.env.PORT || 4242;
+const historySize = 50;
 
-let history = []
+let history = [];
 
-// Serveer client-side bestanden
-app.use(express.static(path.resolve('public')))
+// Serve static files from the "public" directory
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-// Start de socket.io server op
+// Serve the index.html file for the root path
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+// Start the socket.io server
 ioServer.on('connection', (client) => {
-  // Log de connectie naar console
-  console.log(`user ${client.id} connected`)
+  console.log(`user ${client.id} connected`);
+  client.emit('history', history);
 
-  // Stuur de history
-  client.emit('history', history)
-
-  // Luister naar een message van een gebruiker
   client.on('message', (message) => {
-    // Check de maximum lengte van de historie
     while (history.length > historySize) {
-      history.shift()
+      history.shift();
     }
-    // Voeg het toe aan de historie
-    history.push(message)
+    history.push(message);
+    ioServer.emit('message', message);
+  });
 
-    // Verstuur het bericht naar alle clients
-    ioServer.emit('message', message)
-  })
-
-  // Luister naar een disconnect van een gebruiker
   client.on('disconnect', () => {
-    // Log de disconnect
-    console.log(`user ${client.id} disconnected`)
-  })
-})
+    console.log(`user ${client.id} disconnected`);
+  });
+});
 
-// Start een http server op het ingestelde poortnummer en log de url
+// Start the http server
 http.listen(port, () => {
-  console.log('listening on http://localhost:' + port)
-})
+  console.log('listening on http://localhost:' + port);
+});
