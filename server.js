a2722d9c8abdc4b61b7e7d2f17781391b5cedb4a
Inju Michorius <inju@hotmail.nl>
2024-03-27 16:03:59 +0100

    Word scramble refresh fix

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const { Server } = require('socket.io');
-const { createServer } = require('http');
-const express = require('express');
+const path = require("path");
+const { Server } = require("socket.io");
+const { createServer } = require("http");
+const express = require("express");
 
-const { shuffleWord } = require('./utils/shuffleWord');
-const { getRandomWordWithHint } = require('./utils/getRandomWordWithHint');
+const { shuffleWord } = require("./utils/shuffleWord");
+const { getRandomWordWithHint } = require("./utils/getRandomWordWithHint");
 
-const formatMessage = require('./utils/formatMessage')
+const formatMessage = require("./utils/formatMessage");
 const {
   userJoin,
   getCurrentUser,
@@ -24,45 +24,32 @@ const ioServer = new Server(http, {
 });
 const port = process.env.PORT || 4242;
 const botName = "Scramble master";
-let refreshInterval
-let randomWord
-// Map to store room states (whether a game is in progress)
-const roomStates = new Map();
-
-// Function to start the game for a room
-function startGame(room) {
-  roomStates.set(room, true); // Set room state to indicate game in progress
-  const emitRandomWord = () => {
-    const { randomWord: newRandomWord, randomHint } = getRandomWordWithHint();
-    randomWord = newRandomWord; // Update the value of randomWord
-    let shuffledWord = shuffleWord(randomWord);
-    ioServer.to(room).emit("randomWord", shuffledWord);
-  };
-
-  // Refresh the random word every 5 seconds
-  refreshInterval = setInterval(emitRandomWord, 10000);
-
-  // Stop the game after 2 minutes (optional)
-  setTimeout(() => {
-    clearInterval(refreshInterval);
-    roomStates.delete(room); // Clear room state after game ends
-  }, 2 * 60 * 1000);
-}
+let randomWord;
 
 // Serve static files from the "public" directory
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, "public")));
 
 // Serve the index.html file for the root path
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
 // Start the socket.io server
-ioServer.on('connection', (client) => {
+ioServer.on("connection", (client) => {
   console.log(`user ${client.id} connected`);
 
-
   client.on("joinRoom", ({ username, room }) => {
+    const emitRandomWord = () => {
+      const { randomWord: newRandomWord, randomHint } = getRandomWordWithHint();
+      randomWord = newRandomWord; // Update the value of randomWord
+      let shuffledWord = shuffleWord(randomWord);
+      console.log(shuffledWord);
+      ioServer.to(room).emit("randomWord", shuffledWord);
+    };
+    //Start game by refreshing the random word every 5 seconds
+    if (getRoomUsers(room).length === 0) {
+      refreshInterval = setInterval(emitRandomWord, 3000);
+    }
     const user = userJoin(client.id, username, room);
     client.join(user.room);
 
@@ -76,59 +63,68 @@ ioServer.on('connection', (client) => {
         "message",
         formatMessage(botName, `${user.username} has joined the chat`)
       );
-
+    const roomUsers = getRoomUsers(user.room);
     // Send users and room info
     ioServer.to(user.room).emit("roomUsers", {
       room: user.room,
       users: getRoomUsers(user.room),
     });
-
-    // Check if the room has an ongoing game
-    if (!roomStates.has(room)) {
-      // Start the game for the first player
-      startGame(room);
-    }
+    console.log(`Number of users in room ${user.room}: ${roomUsers.length}`);
   });
 
-  client.on('message', (message) => {
+  client.on("message", (message) => {
     const user = getCurrentUser(client.id);
 
     if (message.toLowerCase() === randomWord) {
       user.points += 100;
-      ioServer.to(user.room).emit("message", formatMessage(user.username, ' has guessed the word!'));
+      ioServer
+        .to(user.room)
+        .emit(
+          "message",
+          formatMessage(user.username, " has guessed the word!")
+        );
       ioServer.to(user.room).emit("roomUsers", {
         room: user.room,
         users: getRoomUsers(user.room),
       });
     } else {
-      ioServer.to(user.room).emit("message", formatMessage(user.username, message));
+      ioServer
+        .to(user.room)
+        .emit("message", formatMessage(user.username, message));
     }
   });
 
-  //on disconnect
+  // Handle disconnect event
   client.on("disconnect", () => {
-    console.log(`user ${client.id} connected`);
+    console.log(`user ${client.id} disconnected`);
     const user = userLeave(client.id);
 
     if (user) {
-      ioServer.to(user.room).emit(
-        "message",
-        formatMessage(botName, `${user.username} has left the chat`)
-      );
-
+      ioServer
+        .to(user.room)
+        .emit(
+          "message",
+          formatMessage(botName, `${user.username} has left the chat`)
+        );
+      const roomUsers = getRoomUsers(user.room);
+      if (roomUsers.length === 0) {
+        // Stop refreshing the random word every 5 seconds
+        clearInterval(refreshInterval);
+      }
+      
       // Send users and room info
       ioServer.to(user.room).emit("roomUsers", {
         room: user.room,
         users: getRoomUsers(user.room),
       });
-    }
-    //TODO: figure out this questionable code
-    clearInterval(refreshInterval);
-  });
 
+      // Log the number of users in the room after user leaves
+      console.log(`Number of users in room ${user.room}: ${roomUsers.length}`);
+    }
+  });
 });
 
 // Start the http server
 http.listen(port, () => {
-  console.log('listening on http://localhost:' + port);
+  console.log("listening on http://localhost:" + port);
 });
